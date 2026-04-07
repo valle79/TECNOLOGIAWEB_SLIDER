c4b7d7ecd0742de365a931dc8ee373cff8849c68
LuisValle79 <luis.valle@vallegrande.edu.pe>
2026-04-07 17:43:49 -0500

    clean main branch

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Database models for Wine E-commerce
-"""
-from .database import Database
-from .wine import Wine
-from .order import Order
-from .user import User
-
-__all__ = ['Database', 'Wine', 'Order', 'User']
--- services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Business logic services
-"""
-from .cart_service import CartService
-from .order_service import OrderService
-
-__all__ = ['CartService', 'OrderService']
